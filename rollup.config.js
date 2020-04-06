2efb9c683fb09a2da487bd14ad4fc4a7c9ef0d2e
UditKumarChawla <Uditkumar912@gmail.com>
2020-04-06 15:01:52 +0530

    feat(object): object format changes

--- rollup.config.js
+++ rollup.config.js
@@ -4,17 +4,27 @@ export default [
     input: "src/core.js",
     output: {
       file: "dist/abc.js",
-      format: "iife"
+      format: "iife",
     },
     plugins: [
       myExample({
         files: {
-            "dist/abc.js": ["//We are here","dist/abc.js"],
-            "temp/abcd.js": ["//We are here","dist/abc.js"],
-            "temp/temp.js": ["//We are here","dist/abc.js"]
+            "dist/abc.js": {
+              banner:["nefbbeegibg"],
+              concatFiles:['src/init.js'],
+              footer:["fnnwfiunui"]
+            },
+            "dist/dist2/abcd.js": {
+              banner:["nefbbeegibg"],
+              concatFiles:['src/core.js'],
+              footer:["fnnwfiunui"]
+            },
+            "dist/dist2/dist3/abcde.js": {
+              banner:["hello"],
+              concatFiles:['dist/abc.js'],
+              footer:["miketesting123 "]
+            },
         }
-        // banner: ["hey", "src/init.js"],
-        // footer: ["src/core.js"]
       })
     ]
   }
